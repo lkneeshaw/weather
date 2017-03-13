b8dc08cf58b7969fa83780f578f0070a86f1526e
lkneeshaw <lkneeshaw@vcd-ceb131-22.eastern.ewu.edu>
2017-03-13 09:42:45 -0700

    Spokane Weather
    * added weather from codepen
    * html, css, and script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-// YOUR SCRIPTS GO HERE
+// GET THAT WEATHER
+
+$.simpleWeather({
+  location: 99223,
+  unit: 'f',
+  success: function(weather){
+      
+      // Weather Check
+      console.log(weather);
+    
+      // Display Data
+      $('.city').text(weather.city);
+      $('.state').text(weather.region);
+      $('.temp').text(weather.temp);
+      $('.thumb img').attr('src', weather.image);
+    
+    },
+  error:  function(error){
+      console.log('No Weather Found'); 
+    }
+});
