4d35c9a73436a2ca1a2ce1646741121a95f4ccec
lkneeshaw <lrkneeshaw@yahoo.com>
2017-03-23 18:01:31 -0700

    Added weather font, bg conditionals
    * created scss folder and files for global, fonts, and media queries
    * added weather icon font
    * added js and css for conditional backgrounds

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,51 @@
 // GET THAT WEATHER
 
+// Note: Plugin has been updated. Use version 3.1
+// https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+// Refer: http://simpleweatherjs.com
+
 $.simpleWeather({
-  location: 99223,
-  unit: 'f',
-  success: function(weather){
-      
-      // Weather Check
+    location: 99223,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
       console.log(weather);
-    
+      
       // Display Data
-      $('.city').text(weather.city);
-      $('.state').text(weather.region);
       $('.temp').text(weather.temp);
-      $('.thumb img').attr('src', weather.image);
-    
+      $('.city').text(weather.city);
+      $('i').addClass( 'icon-' + weather.code);
+      
+      // Condition Code
+      // Reference Condition Codes
+      // https://developer.yahoo.com/weather/documentation.html#codes
+      console.log(weather.code);
+      
+      // If Sunny `code` between 31 and 36       
+      if (weather.code >= 31 && weather.code <= 36 ) {
+        
+        $('body').addClass('sunny');
+        
+      }
+      
+       // If Cloudy `code` between 31 and 36       
+      if (weather.code >= 26 && weather.code <= 30 ) {
+        
+        $('body').addClass('cloudy');
+        
+      }
+      
+      // If Rainy `code` between 31 and 36       
+      if (weather.code >= 5 && weather.code <= 14 ) {
+        
+        $('body').addClass('rainy');
+        
+      }
+      
     },
-  error:  function(error){
-      console.log('No Weather Found'); 
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
     }
-});
+  
+  });
